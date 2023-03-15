cbc0ad466b93541aee7985aee53babe0645fb77c
aysenuriye <118122386+aysenuriye@users.noreply.github.com>
2023-03-15 21:39:12 +0300

    21:39

--- src/test/java/hooks/Hoooks.java
+++ src/test/java/hooks/Hoooks.java
@@ -1,20 +1,18 @@
-package stepdefinitions;
+package hooks;
 
-import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import utilities.Driver;
 
-public class Hooks {
-    @After
+public class Hoooks {
+    @AfterStep
     public void tearDown(Scenario scenario){
 
-        final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
         if (scenario.isFailed()) {
+            final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
             scenario.attach(screenshot, "image/png","screenshots");
         }
-        Driver.closeDriver();
     }
-
 }
